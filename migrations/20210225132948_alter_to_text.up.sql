ed0e8280bf061bb2f40fb572df34fd5c5fdeaa9b
idegart <degtyarev_artem@mail.ru>
2021-02-25 13:30:37 +0300

    Fix for long text

---
+++ migrations/20210225132948_alter_to_text.up.sql
@@ -0,0 +1,3 @@
+ALTER TABLE daily_user_reports ALTER COLUMN done TYPE text;
+ALTER TABLE daily_user_reports ALTER COLUMN will_do TYPE text;
+ALTER TABLE daily_user_reports ALTER COLUMN blocker TYPE text;
