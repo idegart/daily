137c0e55839010a045bfa7d0405e3eeca81a690c
idegart <degtyarev_artem@mail.ru>
2021-03-15 11:28:32 +0300

    Small fix

--- internal/model/user.go
+++ internal/model/user.go
@@ -1,17 +1,14 @@
 package model
 
-import (
-	"database/sql"
-	"time"
-)
+import "time"
 
 type User struct {
-	Id         int           `json:"id" db:"id"`
-	Email      string        `json:"email" db:"email"`
-	Name       string        `json:"name" db:"name"`
-	AirtableId sql.NullInt64 `json:"airtable_id" db:"airtable_id"`
-	SlackId    string        `json:"slack_id" db:"slack_id"`
-	Emoji      string        `json:"emoji" db:"emoji"`
-	CreatedAt  time.Time     `json:"created_at" db:"created_at"`
-	UpdatedAt  time.Time     `json:"updated_at" db:"updated_at"`
+	Id         int       `json:"id" db:"id"`
+	Email      string    `json:"email" db:"email"`
+	Name       string    `json:"name" db:"name"`
+	AirtableId *int       `json:"airtable_id" db:"airtable_id"`
+	SlackId    string    `json:"slack_id" db:"slack_id"`
+	Emoji      string    `json:"emoji" db:"emoji"`
+	CreatedAt  time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at" db:"updated_at"`
 }
